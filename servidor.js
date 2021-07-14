7cb4eb029016d95d17113cbb6cccd459e7e7a6ef
unknown <guuiii2548@gmail.com>
2021-07-13 22:55:39 -0300

    Se conectou com o banco e deixou algo para depois ...

--- servidor.js
+++ servidor.js
@@ -1,6 +1,8 @@
+//Importando express e iniciando Servidor//
 const express = require('express')
 servidor = express()
 
+//Rotas//
 servidor.set('view engine','ejs')
 
 servidor.get('/',(req,res)=>{
@@ -11,6 +13,38 @@ servidor.get('/registro',(req,res)=>{
     res.render('registro.ejs')
 })
 
+servidor.get('/login/search',(req,res)=>{
+
+})
+
+servidor.get('/registro/new',(req,res)=>{
+    
+})
+
+//Servidor ouvindo na porta 3000//
 servidor.listen(3000,()=>{
     console.log('servidor de pé')
 })
+
+//FrontEnd Dados//
+
+
+
+
+
+//Banco de dados//
+const recebidoE = //Receber o email nessa variável e validar abaixo//
+const recebidoS = //Receber a Senha nessa variável e validar abaixo//
+
+//Api MongoDB//
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/Aplicativo')
+const usuario = mongoose.model('usuarios',{email:String,senha:String})
+const novo = new usuario({"email":recebidoE,"senha":recebidoS})
+
+/*novo.save(function(err){
+    if (err) throw err
+    console.log('inserido')
+})*/          //Rodar essa Função quando ele quiser criar um usuário//
+
+/*novo.find*/  //Rodar essa função quando ele quiser logar ou criar um usuário//
